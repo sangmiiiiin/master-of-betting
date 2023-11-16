a015af719433552ac12e1357525693cafb5429f1
SANGMIN <kwk680037@gmail.com>
2023-11-16 22:28:14 +0900

    💄 Add image file, header  and Main UI (이미지 파일 추가, 헤더 메인 UI 추가)

--- src/Layout/header/header.js
+++ src/Layout/header/header.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import '../../scss/main.scss';
- 
+import '../../scss/header.scss';
 
 export const Header = () => {
-    return(
-      <></>
-    )
+  return (
+    <div className='headerContainer'>
+      <header>
+        <div className="background">
+          <h1>내기의 달인</h1>
+          <p>
+            간단한 게임으로 복불복!<br />
+            승자와 패자를 결정해보세요!
+          </p>
+        </div>
+      </header>
+    </div>
+  )
 }
--- src/pages/mainPage/index.js
+++ src/pages/mainPage/index.js
@@ -1,7 +1,34 @@
+import { Card, CardBody, CardImg, CardText, CardTitle } from "react-bootstrap"
+import { Header } from "../../Layout/header/header"
+import exampleImg from'../../image/gameImg.jpg'
+import '../../scss/mainpage.scss';
+
 export const Index = () => {
     return(
         <>
+            <Header></Header>
             
+            <main>
+                <Card className="card">
+                    <CardImg variant="top" src={exampleImg} alt="Card image" />
+                    <CardBody>
+                        <CardTitle>사다리타기</CardTitle>
+                        <CardText>
+                            사다리타기를 통해 벌칙자를 결정해보세요!
+                        </CardText>
+                    </CardBody>
+                </Card>
+
+                <Card className="card">
+                    <CardImg variant="top" src={exampleImg} alt="Card image" />
+                    <CardBody>
+                        <CardTitle>사다리타기</CardTitle>
+                        <CardText>
+                            사다리타기를 통해 벌칙자를 결정해보세요!
+                        </CardText>
+                    </CardBody>
+                </Card>
+            </main>
         </>
     )
 }
