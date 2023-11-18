db14bae1c7bafdcfef44732dd95ad4c5602ae870
SANGMIN <kwk680037@gmail.com>
2023-11-18 22:05:50 +0900

    💄 Modify main page UI (메인페이지 UI 수정)

--- src/pages/mainPage/index.js
+++ src/pages/mainPage/index.js
@@ -12,8 +12,8 @@ export const Index = () => {
             <main>
                 <Link to="/ladder">
                     <Card className="card">
-                        <CardImg variant="top" src={exampleImg} alt="Card image" />
-                        <CardBody>
+                        <CardImg className="cardImg" variant="top" src={exampleImg} alt="Card image" />
+                        <CardBody className="cardBody">
                             <CardTitle>사다리타기</CardTitle>
                             <CardText className="cardText">
                                 사다리타기를 통해 벌칙자를 결정해보세요!
@@ -24,15 +24,39 @@ export const Index = () => {
 
                 <Link to="/drowlots">
                     <Card className="card">
-                        <CardImg variant="top" src={exampleImg} alt="Card image" />
-                        <CardBody>
+                        <CardImg className="cardImg" variant="top" src={exampleImg} alt="Card image" />
+                        <CardBody className="cardBody">
                             <CardTitle>제비뽑기</CardTitle>
                             <CardText className="cardText">
                                 제비뽑기를 통해 벌칙자를 결정하세요!
                             </CardText>
                         </CardBody>
                     </Card>
-                </Link>               
+                </Link>
+
+                <Link to="/drowlots">
+                    <Card className="card">
+                        <CardImg className="cardImg" variant="top" src={exampleImg} alt="Card image" />
+                        <CardBody className="cardBody">
+                            <CardTitle>제비뽑기</CardTitle>
+                            <CardText className="cardText">
+                                제비뽑기를 통해 벌칙자를 결정하세요!
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Link>         
+
+                <Link to="/drowlots">
+                    <Card className="card">
+                        <CardImg className="cardImg" variant="top" src={exampleImg} alt="Card image" />
+                        <CardBody className="cardBody">
+                            <CardTitle>제비뽑기</CardTitle>
+                            <CardText className="cardText">
+                                제비뽑기를 통해 벌칙자를 결정하세요!
+                            </CardText>
+                        </CardBody>
+                    </Card>
+                </Link>                        
             </main>
         </>
     )
